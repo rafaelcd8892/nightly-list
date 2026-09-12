ea7cd8bc91c7bd0dc454db974693599e7d5b8685
rafaelcd8892 <69434509+rafaelcd8892@users.noreply.github.com>
2026-09-11 21:11:26 -0500

    Initial Commit

---
+++ Recordatorios/RecordatoriosApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordatoriosApp.swift
+//  Recordatorios
+//
+//  Created by Rafael on 11/09/26.
+//
+
+import SwiftUI
+
+@main
+struct RecordatoriosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
